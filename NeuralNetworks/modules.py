c08f337609d0a89a7d95dbb5d6fb549c98b9b92d
Roman <seleznyov.romario@mail.ru>
2024-12-26 16:50:25 +0300

    adding finished modules

--- NeuralNetworks/modules.py
+++ NeuralNetworks/modules.py
@@ -1,44 +1,44 @@
 import numpy as np
 
-
 class Module(object):
     """
     Basically, you can think of a module as of a something (black box)
     which can process `input` data and produce `ouput` data.
     This is like applying a function which is called `forward`:
 
-    output = module.forward(input)
+        output = module.forward(input)
 
     The module should be able to perform a backward pass: to differentiate the `forward` function.
     More, it should be able to differentiate it if is a part of chain (chain rule).
     The latter implies there is a gradient from previous step of a chain rule.
 
-    gradInput = module.backward(input, gradOutput)
+        gradInput = module.backward(input, gradOutput)
     """
-    def __init__(self):
+    def __init__ (self):
         self.output = None
         self.gradInput = None
         self.training = True
 
-    def forward(self, input_data):
+    def forward(self, input):
         """
         Takes an input object, and computes the corresponding output of the module.
         """
-        return self.update_output(input_data)
+        return self.updateOutput(input)
 
-    def backward(self, input_data, grad_output):
+    def backward(self, input, gradOutput):
         """
         Performs a backpropagation step through the module, with respect to the given input.
 
         This includes
-        - computing a gradient w.r.t. `input` (is needed for further backprop),
-        - computing a gradient w.r.t. parameters (to update parameters while optimizing).
+         - computing a gradient w.r.t. `input` (is needed for further backprop),
+         - computing a gradient w.r.t. parameters (to update parameters while optimizing).
         """
-        self.update_grad_input(input_data, grad_output)
-        self.acc_grad_parameters(input_data, grad_output)
+        self.updateGradInput(input, gradOutput)
+        self.accGradParameters(input, gradOutput)
         return self.gradInput
 
-    def update_output(self, input_data):
+
+    def updateOutput(self, input):
         """
         Computes the output using the current parameter set of the class and input.
         This function returns the result which is stored in the `output` field.
@@ -50,17 +50,18 @@ class Module(object):
 
         # self.output = input
         # return self.output
+
         pass
 
-    def update_grad_input(self, input_data, grad_output):
+    def updateGradInput(self, input, gradOutput):
         """
-                Computing the gradient of the module with respect to its own input.
-                This is returned in `gradInput`. Also, the `gradInput` state variable is updated accordingly.
+        Computing the gradient of the module with respect to its own input.
+        This is returned in `gradInput`. Also, the `gradInput` state variable is updated accordingly.
 
-                The shape of `gradInput` is always the same as the shape of `input`.
+        The shape of `gradInput` is always the same as the shape of `input`.
 
-                Make sure to both store the gradients in `gradInput` field and return it.
-                """
+        Make sure to both store the gradients in `gradInput` field and return it.
+        """
 
         # The easiest case:
 
@@ -69,20 +70,27 @@ class Module(object):
 
         pass
 
-    def acc_grad_parameters(self, input_data, grad_output):
+    def accGradParameters(self, input, gradOutput):
         """
         Computing the gradient of the module with respect to its own parameters.
         No need to override if module has no parameters (e.g. ReLU).
         """
         pass
 
-    def zero_grad_parameters(self):
+    def zeroGradParameters(self):
         """
         Zeroes `gradParams` variable if the module has params.
         """
         pass
 
-    def get_grad_parameters(self):
+    def getParameters(self):
+        """
+        Returns a list with its parameters.
+        If the module does not have parameters return empty list.
+        """
+        return []
+
+    def getGradParameters(self):
         """
         Returns a list with gradients with respect to its parameters.
         If the module does not have parameters return empty list.
@@ -109,3 +117,152 @@ class Module(object):
         to have readable description.
         """
         return "Module"
+
+
+class Sequential(Module):
+    """
+         This class implements a container, which processes `input` data sequentially.
+
+         `input` is processed by each module (layer) in self.modules consecutively.
+         The resulting array is called `output`.
+    """
+
+    def __init__ (self):
+        super(Sequential, self).__init__()
+        self.modules = []
+
+    def add(self, module):
+        """
+        Adds a module to the container.
+        """
+        self.modules.append(module)
+
+    def updateOutput(self, input):
+        """
+        Basic workflow of FORWARD PASS:
+
+            y_0    = module[0].forward(input)
+            y_1    = module[1].forward(y_0)
+            ...
+            output = module[n-1].forward(y_{n-2})
+
+        Just write a little loop.
+        """
+
+        self.output = input
+
+        for module in self.modules:
+            self.output = module.forward(self.output)
+
+        return self.output
+
+    def backward(self, input, gradOutput):
+        """
+        Workflow of BACKWARD PASS:
+
+            g_{n-1} = module[n-1].backward(y_{n-2}, gradOutput)
+            g_{n-2} = module[n-2].backward(y_{n-3}, g_{n-1})
+            ...
+            g_1 = module[1].backward(y_0, g_2)
+            gradInput = module[0].backward(input, g_1)
+
+        !!!
+
+        To ech module you need to provide the input, module saw while forward pass,
+        it is used while computing gradients.
+        Make sure that the input for `i-th` layer the output of `module[i]` (just the same input as in forward pass)
+        and NOT `input` to this Sequential module.
+
+        !!!
+        """
+        for i in range(len(self.modules)-1, 0, -1):
+            gradOutput = self.modules[i].backward(self.modules[i-1].output, gradOutput)
+
+        self.gradInput = self.modules[0].backward(input, gradOutput)
+
+        return self.gradInput
+
+
+    def zeroGradParameters(self):
+        for module in self.modules:
+            module.zeroGradParameters()
+
+    def getParameters(self):
+        """
+        Should gather all parameters in a list.
+        """
+        return [x.getParameters() for x in self.modules]
+
+    def getGradParameters(self):
+        """
+        Should gather all gradients w.r.t parameters in a list.
+        """
+        return [x.getGradParameters() for x in self.modules]
+
+    def __repr__(self):
+        string = "".join([str(x) + '\n' for x in self.modules])
+        return string
+
+    def __getitem__(self,x):
+        return self.modules.__getitem__(x)
+
+    def train(self):
+        """
+        Propagates training parameter through all modules
+        """
+        self.training = True
+        for module in self.modules:
+            module.train()
+
+    def evaluate(self):
+        """
+        Propagates training parameter through all modules
+        """
+        self.training = False
+        for module in self.modules:
+            module.evaluate()
+
+
+class Criterion(object):
+    def __init__ (self):
+        self.output = None
+        self.gradInput = None
+
+    def forward(self, input, target):
+        """
+            Given an input and a target, compute the loss function
+            associated to the criterion and return the result.
+
+            For consistency this function should not be overrided,
+            all the code goes in `updateOutput`.
+        """
+        return self.updateOutput(input, target)
+
+    def backward(self, input, target):
+        """
+            Given an input and a target, compute the gradients of the loss function
+            associated to the criterion and return the result.
+
+            For consistency this function should not be overrided,
+            all the code goes in `updateGradInput`.
+        """
+        return self.updateGradInput(input, target)
+
+    def updateOutput(self, input, target):
+        """
+        Function to override.
+        """
+        return self.output
+
+    def updateGradInput(self, input, target):
+        """
+        Function to override.
+        """
+        return self.gradInput
+
+    def __repr__(self):
+        """
+        Pretty printing. Should be overrided in every module if you want
+        to have readable description.
+        """
+        return "Criterion"
